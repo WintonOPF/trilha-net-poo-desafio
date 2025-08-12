4467a9e1320f4a417beb7cce88c79625f7f1ff0b
WintonOPF <wintonfilho@ufms.br>
2025-08-11 22:52:48 -0400

    Solução.

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,21 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    public class Nokia : Smartphone
     {
+        public Nokia(string numero) : base(numero) { }
+
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            try
+            {
+                Console.WriteLine($"Instalando o aplicativo {nomeApp} no Nokia.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Erro ao instalar o aplicativo: {ex.Message}");
+            }
+        }
     }
 }
